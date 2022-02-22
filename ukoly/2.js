007f85bc90c98b3cf206662f299dd6d63ca776b9
Aliasih <rolencova.a@gmail.com>
2022-02-22 19:05:56 +0100

    1 file modified, 1 added

--- ukoly/2.js
+++ ukoly/2.js
@@ -5,6 +5,20 @@ let jmeno = prompt("Jak se jmenuješ?");
 console.log("Jmenuješ se:" + jmeno);
 
 
+let vek = 18;
+
+//Jsi plnoletý
+//NEjsi plnoletý
+
+//TRue/FALSE
+
+//if(VYRAZ) {
+  // vykona se, pokud je vyraz pravdivy
 
 
-
+if (vek >= 18) {
+  console.log("Vyborne, jsi plnoletý");
+  console.log("ahoj");
+}else {
+  console.log("Nejsi plnoletý");
+}
