bd64e21aa62e799a3c2df3288120d24b154aaa9a
Aliasih <rolencova.a@gmail.com>
2022-02-22 18:14:56 +0100

    1 file modified

--- ukoly/1.js
+++ ukoly/1.js
@@ -4,12 +4,13 @@ let jmeno = "Petra";
 let věk = 34;
 let vaha = 65;
 let výška = 1.67;
-let BMI = 23.2
+let bmi = 23.2
 
 //vzorecek pro BMI
 let bmi = vaha / (1.67 * 1.67);
 
 //Zápis do konzole
-console.log("Jmenuji se" + jmeno + "Je mi" věk + "Vážím" vaha + "Měřím" výška);
+console.log("Jmenuji se" + jmeno + "Je mi" věk + "Vážím" + vaha + "Měřím" + výška + "Moje BMI je" + bmi );
+
 
 
