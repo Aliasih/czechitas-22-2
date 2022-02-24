8e8f702ec521c89b03b0839e3646b2d7c9d5112f
Aliasih <rolencova.a@gmail.com>
2022-02-24 16:34:58 +0100

    1 file modified, 1 added

---
+++ ukoly/3.js
@@ -0,0 +1,11 @@
+let jmeno = prompt("Jak se jmenuješ?");
+let vek = Number(prompt("jaky je vas vek?");
+
+
+if (vek >= 18) {
+  console.log(`Vaše jméno je ${jmeno}, je vám ${vek}');
+  
+}else {
+  console.log("Nejsi plnoletý");}
+
+
---
+++ ukoly/4.js
@@ -0,0 +1,33 @@
+let a = Number(prompt("Zadejte stranu A:"));
+let b = Number(prompt("Zadejte stranu B:"));
+let c = Number(prompt("Zadejte stranu C:"));
+
+if (a>b) {
+  if (a>c) {
+    if (b = a) {
+
+    }
+  }
+
+}
+
+
+let a = Number(prompt('Zadej číslo A:'));
+let b = Number(prompt('Zadej číslo B:'));
+let c = Number(prompt('Zadej číslo C:'));
+
+if (a > b) {
+  if (a > c) {
+    console.log('Největší číslo je číslo A:' + a);
+  } else {
+    console.log('Největší číslo je číslo C:' + c);
+  }
+} else {
+  if (c < b) {
+    console.log('Největší je číslo B:' + b);
+  } else {
+    console.log('Největší je číslo C:' + c);
+  }
+}
+
+
