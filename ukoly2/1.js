6be95d7bfd49ce395223b2089c4ceb473c6375ba
Aliasih <rolencova.a@gmail.com>
2022-02-24 17:15:01 +0100

    1 file modified

--- ukoly2/1.js
+++ ukoly2/1.js
@@ -1,15 +1,3 @@
-//JE třeba načíst číslo kvůli orientaci
-let cislo = Number(prompt("Zadej číslo odlišné od 0"));
-
-while ( cislo == 0) {
-  console.log("Prosím zadejte číslo jiné než je nula");
-  cislo = Number(prompt("zadej"));
-}
-
-console.log("Vaše zadané číslo je: " + cislo)
-
-
-
 
 
 // čti tak dlouho, dokud nezadá 0 a vždy vypiš NAKUMULOVANÝ SOUČET ČÍSEL
