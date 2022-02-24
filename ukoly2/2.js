2913a22d503270b47cfb51f8f77a52c4de960d84
Aliasih <rolencova.a@gmail.com>
2022-02-24 18:33:26 +0100

    1 file modified, 1 added

---
+++ ukoly2/2.js
@@ -0,0 +1,24 @@
+//UKOL FOR 1
+
+//Načteme horní a dolní mez
+let dolni = Number(prompt("Dolní mez: "));
+let horni = Number(prompt("Horní mez: "));
+
+//Projíždíme čísla
+
+for (let i = dolni; i <= horni; i++) {
+  console.log(i);
+}
+
+// ÚKOL FOR 2
+
+let horni = Number(prompt("Horni mez: "));
+
+for (let i = 1, i <= horni; i++) {
+  if(i % 2 == 0) {
+    console.log (i);
+  }
+}
+
+//ÚKOL FOR 3
+
