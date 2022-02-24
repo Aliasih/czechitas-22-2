80b3e5dd5d5f7a9d7bec69987efc4b38cd5ee96f
Aliasih <rolencova.a@gmail.com>
2022-02-24 17:14:14 +0100

    1 file modified

--- ukoly2/1.js
+++ ukoly2/1.js
@@ -1,3 +1,4 @@
+//JE třeba načíst číslo kvůli orientaci
 let cislo = Number(prompt("Zadej číslo odlišné od 0"));
 
 while ( cislo == 0) {
@@ -6,3 +7,27 @@ while ( cislo == 0) {
 }
 
 console.log("Vaše zadané číslo je: " + cislo)
+
+
+
+
+
+// čti tak dlouho, dokud nezadá 0 a vždy vypiš NAKUMULOVANÝ SOUČET ČÍSEL
+
+let cislo = Number(prompt("Zadej"));
+
+//AKUMULAČNÍ SOUČET do něj budem přičítat
+
+let soucet = 0;
+
+//Dokud uživatel nezadá nulu tak pokračujeme
+
+while (cislo != 0) {
+  console.log("Vstup: " + cislo);
+  soucet += cislo;
+  console.log("Naakumulovaný soucet: " + soucet);
+  cislo = Number(prompt("Zadej:")) 
+  // znovu while cyklus 
+}
+
+console.log.("Finalni soucet: " + soucet);
